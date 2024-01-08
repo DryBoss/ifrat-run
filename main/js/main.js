a953086d74c26c186188acda90fcdc2fe1b6183e
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2024-01-08 17:59:10 +0600

    progress

--- main/js/main.js
+++ main/js/main.js
@@ -1,8 +1,15 @@
 const world = document.querySelector(".world");
 const grounds = document.querySelectorAll(".ground");
 const playButton = document.querySelector(".play");
+const jumpButton = document.querySelector(".jump");
+const duckButton = document.querySelector(".duck");
+const char = document.querySelector(".char");
 
 let gameActive = false;
+let gameSpeed;
+let score;
+let frameCount;
+let charJump = false;
 
 const getPropertyValue = (element, property) => {
   return parseFloat(getComputedStyle(element).getPropertyValue(property)) || 0
@@ -20,21 +27,49 @@ const updateGround = (speed) => {
       increasePropertyValue(ground, "--left", speed * -1)
   
       if (getPropertyValue(ground, "--left") <= -300) {
+        score++;
+        gameSpeed = gameSpeed + 0.01;
+        console.log(score)
         increasePropertyValue(ground, "--left", 600)
       }
     })
   } else {
     playButton.classList.remove("hide");
+    gameSpeed = 0.5;
+    score = 0
+  }
+}
+
+const updateDino = (speed) => {
+  if (gameActive && !charJump) {
+    if (frameCount <= 5) {
+      char.src = 'images/char-run-1.png'
+    } else {
+      char.src = 'images/char-run-2.png'
+    }
+  } else {
+    char.src = 'images/char-stand.png'
   }
 }
 
 playButton.addEventListener('click', () => {
   playButton.classList.add("hide");
   gameActive = true;
+  frameCount = 0;
 });
+jumpButton.addEventListener('click', () => {
+  char.src = 'images/char-stand.png';
+  charJump = true;
+})
 
 const updateGame = () => {
-  updateGround(1);
+  updateGround(gameSpeed);
+  updateDino(gameSpeed);
+  if (frameCount > 10) {
+    frameCount = 0
+  } else {
+    frameCount++
+  }
 };
 
 setPropertyValue(grounds[0], "--left", 0)
